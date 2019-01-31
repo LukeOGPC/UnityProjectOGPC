2fdf5d2fa3ac80fa8e9c32bdd5dbc5f10218fbdd
Luke Williamson <lukewilliamson2001@gmail.com>
2019-01-31 15:10:28 -0800

    working again

---
+++ Assets/Project/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour 
+{
+	private PlayerController player;
+	UnityEngine.AI.NavMeshAgent agent;
+
+	void Start () 
+	{
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		player = FindObjectOfType<PlayerController> ();
+	}
+
+	void Update () 
+	{
+		agent.SetDestination (player.transform.position);
+	}
+}
